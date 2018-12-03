413de0409720a4318c13157ff0cbec0463703eee
Luiz Cunha <luiz.cunha@netlex.com.br>
2018-12-03 18:31:33 -0200

    Início do desenvolvimento do tabuleiro - Criação de Peças e tabuleiro

---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tab = tab;
+            this.qteMovimentos = 0;
+        }
+    }
+}
