34911968ffc0c48f272f43081ee927088b7e52af
Luiz Cunha <luiz.cunha@netlex.com.br>
2018-12-03 18:19:10 -0200

    Início do desenvolvimento do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,12 +11,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine("Posição: "+P);
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
