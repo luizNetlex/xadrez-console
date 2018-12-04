718d0ca4d48db039ab5d8f52ae24920cc2f6c3ab
Luiz Cunha <luiz.cunha@netlex.com.br>
2018-12-04 14:36:34 -0200

    Tratamento de exceções ao colocar peças no tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -12,12 +12,12 @@ namespace xadrez_console
         public static void imprimirTabuleiro(Tabuleiro tab) {
             for (int i = 0; i < tab.linhas; i++) {
                 for (int j = 0; j < tab.colunas; j++) {
-                    if (tab.peca(i, j) == null)
+                    if (tab.peca(new Posicao(i, j)) == null)
                     {
                         Console.Write("- ");
                     }
                     else {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Console.Write(tab.peca(new Posicao(i, j)) + " ");
                     }
                 }
                 Console.WriteLine();
